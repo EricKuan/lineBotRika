7b9a3c5d94589d8b632933af1486267773afe2fc
EricKuan <erickuan@fareastone.com.tw>
2020-01-10 14:31:43 +0800

    modify: 修正remove 使用方式

--- src/main/java/com/fet/lineBot/domain/dao/ReplyRepository.java
+++ src/main/java/com/fet/lineBot/domain/dao/ReplyRepository.java
@@ -2,10 +2,7 @@ package com.fet.lineBot.domain.dao;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.fet.lineBot.domain.model.ReplyMapping;
 
@@ -14,8 +11,5 @@ public interface  ReplyRepository extends CrudRepository<ReplyMapping, Long>{
 	
 	List<ReplyMapping> findByMessage(String message);
 	
-	@Modifying
-	@Query("DELETE FROM LINE_BOT_REPLY_MAPPING L WHERE L.MESSAGE:=message")
-	void deleteMessages(@Param("message")String message);
 
 }

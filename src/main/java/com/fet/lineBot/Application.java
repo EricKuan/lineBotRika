bf456ce1c7c166fe601baaeb652f97a721db668d
EricKuan <erickuan@fareastone.com.tw>
2020-01-10 11:08:19 +0800

    起始版本

---
+++ src/main/java/com/fet/lineBot/Application.java
@@ -0,0 +1,21 @@
+package com.fet.lineBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    
+}
---
+++ src/main/java/com/fet/lineBot/handler/MessageHandler.java
@@ -0,0 +1,29 @@
+package com.fet.lineBot.handler;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@LineMessageHandler
+public class MessageHandler {
+
+	private static String MESSAGE_PREFIX = "call";
+	
+	@EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        String message = event.getMessage().getText();
+        if(0==message.indexOf(MESSAGE_PREFIX)) {
+        	return new TextMessage("なに?");
+        }
+        return new TextMessage("");
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}

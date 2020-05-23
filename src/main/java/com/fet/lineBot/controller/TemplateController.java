9c6e020bbc0477bd0eaaa8318b98a42e80cb86aa
Eric.Kuan <b9413048@gmail.com>
2020-05-23 09:46:02 +0800

    modify: 加入圖片顯示功能

--- src/main/java/com/fet/lineBot/controller/TemplateController.java
+++ src/main/java/com/fet/lineBot/controller/TemplateController.java
@@ -1,5 +1,7 @@
 package com.fet.lineBot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,10 @@ public class TemplateController {
 	@RequestMapping("/helloHtml") 
 	public String hello(Map<String,Object>map) {
 		map.put("hello", "from TemplateController.helloHtml");
+		List<String> imgList  = new ArrayList<String>();
+		imgList.add("https://i.imgur.com/AtgGAta.jpg");
+		imgList.add("https://i.imgur.com/U69o62H.jpg");
+		map.put("imageList", imgList);
 		return "/hello";
 		
 	}

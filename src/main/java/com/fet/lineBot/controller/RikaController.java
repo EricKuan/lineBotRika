c9f01ee1b15ff6fb50897577beea297771ae79e4
Eric.Kuan <b9413048@gmail.com>
2020-05-23 09:04:13 +0800

    add: 加入列出所有會員功能

--- src/main/java/com/fet/lineBot/controller/RikaController.java
+++ src/main/java/com/fet/lineBot/controller/RikaController.java
@@ -1,17 +1,24 @@
 package com.fet.lineBot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fet.lineBot.domain.dao.MemberDataRepository;
+import com.fet.lineBot.domain.model.MemberData;
 import com.fet.lineBot.service.MessageService;
+import com.google.gson.Gson;
 
 @RestController
 public class RikaController {
 
 	@Autowired
 	MessageService messageService;
+	@Autowired
+	MemberDataRepository memberDataRepository;
 	
 	@CrossOrigin
 	@GetMapping(value = "/list", produces = "application/json")
@@ -20,4 +27,14 @@ public class RikaController {
 		rtnMsg = messageService.listMessage();
 		return rtnMsg;
 	}
+	
+	@CrossOrigin
+	@GetMapping(value = "member/list", produces = "application/json")
+	public String listAllMember () {
+		String rtnMsg =null;
+		List<MemberData> allMember = memberDataRepository.findAll();
+		rtnMsg = new Gson().toJson(allMember);
+		return rtnMsg;
+	}
+	
 }

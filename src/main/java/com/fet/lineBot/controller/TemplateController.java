2ecd2f161fc1b5fd1b7893071caa275bc12ab5f7
Eric.Kuan <b9413048@gmail.com>
2020-05-23 12:10:38 +0800

    modify: 嘗試使用某漫畫檔案

--- src/main/java/com/fet/lineBot/controller/TemplateController.java
+++ src/main/java/com/fet/lineBot/controller/TemplateController.java
@@ -13,8 +13,18 @@ public class TemplateController {
 	public String hello(Map<String,Object>map) {
 		map.put("hello", "from TemplateController.helloHtml");
 		List<String> imgList  = new ArrayList<String>();
-		imgList.add("https://i.imgur.com/AtgGAta.jpg");
-		imgList.add("https://i.imgur.com/U69o62H.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/ddba1d66j00q2ov4f003rc000rs01djm.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/771c4a83j00q2ov4f007pc000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/590bf295j00q2ov4e007cc000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/9f8865cfj00q2ov4e005dc000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/686e4581j00q2ov4e008kc000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/183d695bj00q2ov4f007uc000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/1c634be4j00q2ov4e0071c000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/1e325b48j00q2ov4e007ec000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/252e095cj00q2ov4f007jc000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/cc09bcb0j00q2ov4f0069c000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/fd84f7b2j00q2ov4f006wc000rs0186m.jpg");
+		imgList.add("http://dingyue.ws.126.net/2019/1218/aa49e539j00q2ov4e008bc000rs01u6m.jpg");
 		map.put("imgList", imgList);
 		return "/hello";
 		

581833a57688841eb2600c9e449b3e872bd6b00f
EricKuan <erickuan@fareastone.com.tw>
2020-06-04 14:31:11 +0800

    bugfix

--- src/main/java/com/fet/lineBot/controller/RikaController.java
+++ src/main/java/com/fet/lineBot/controller/RikaController.java
@@ -1,10 +1,10 @@
 package com.fet.lineBot.controller;
 
 import java.util.List;
-import org.apache.http.HttpRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -45,7 +45,7 @@ public class RikaController {
 	@CrossOrigin
     @GetMapping(value = "/facebook")
     public ResponseEntity<String> facebook (HttpRequest request) {
-	  logger.info("event: " + new Gson().toJson(request));
+	  logger.info("event: " + new Gson().toJson(request, HttpRequest.class));
         String rtnMsg =null;
         rtnMsg = "Y";
         return new ResponseEntity<>("Hello World!",  HttpStatus.OK);

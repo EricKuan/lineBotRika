3c07835c4f4999b85a759ee9d606f5496c0eeaf6
EricKuan <b9413048@gmail.com>
2021-02-16 14:33:44 +0800

    bugfix: 修正 gson 無法解析問題

--- src/main/java/com/fet/lineBot/domain/model/CheckYoutubeLiveNotifyData.java
+++ src/main/java/com/fet/lineBot/domain/model/CheckYoutubeLiveNotifyData.java
@@ -2,15 +2,17 @@ package com.fet.lineBot.domain.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CheckYoutubeLiveNotifyData {
-    private Map<String, YoutubeLiveData> YOUTUBE_CACHE_MAP_U;
-    private Map<String, Timer> TIMER_CACHE_MAP;
+    private List<YoutubeLiveData> YOUTUBE_CACHE_MAP_U;
     private Date sysdate;
 }
